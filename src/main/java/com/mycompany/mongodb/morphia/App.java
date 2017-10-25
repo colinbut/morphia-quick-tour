30dc8c7869c66f75851a91dc5141b9436dd6782f
Colin But <colinbut@users.noreply.github.com>
2017-10-25 21:34:24 +0100

    setup Morphia

--- src/main/java/com/mycompany/mongodb/morphia/App.java
+++ src/main/java/com/mycompany/mongodb/morphia/App.java
@@ -1,13 +1,23 @@
 package com.mycompany.mongodb.morphia;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.mongodb.MongoClient;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+
+public class App {
+
+    public static void main( String[] args ) {
+
+        final Morphia morphia = new Morphia();
+
+        // tell Morphia where to locate the entities
+        // can be called multiple times with different packages
+        morphia.mapPackage("com.mycompany.mongodb.morphia.entities");
+
+        // Connecting to database default port of 27017 on localhost
+        final Datastore datastore = morphia.createDatastore(new MongoClient(), "morphia-quick-tour");
+
+        datastore.ensureIndexes();
+
     }
 }
