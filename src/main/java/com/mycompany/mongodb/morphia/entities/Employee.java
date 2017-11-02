5e3932c162d91846595b64a0e74700848f31a09a
Colin But <colinbut@users.noreply.github.com>
2017-11-02 00:08:07 +0000

    Added Getters and Setters to entity

--- src/main/java/com/mycompany/mongodb/morphia/entities/Employee.java
+++ src/main/java/com/mycompany/mongodb/morphia/entities/Employee.java
@@ -16,7 +16,7 @@ import org.mongodb.morphia.annotations.Reference;
 
 import java.util.List;
 
-@Entity("employees")
+@Entity(value = "employees", noClassnameStored = true)
 @Indexes(
     @Index(value = "salary", fields = @Field("salary"))
 )
@@ -38,6 +38,42 @@ public class Employee {
 
     public Employee(String name, Double salary) {
         this.name = name;
+        this.salary = salary;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    public void setDirectReports(List<Employee> directReports) {
+        this.directReports = directReports;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
         this.salary = salary;
     }
 
